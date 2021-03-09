af29a1ac47a97eb03a7cc4c0159bc1d67c549e32
Pat Anderson <patrick.b.anderson@gmail.com>
2021-03-09 15:37:57 -0600

    added GET route to server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,16 @@ const express = require( 'express' ); //no ./ because npm installed
 const app = express();
 //tell the server to use "server/public" 
 app.use( express.static( 'server/public' ) );
+//globals
+const port = 5000;
+let devices = [ 'android phone', 'fire tablet' ];
 //tell our app to spin up and listen
-app.listen( 5000, ()=>{
+app.listen( port, ()=>{
     console.log( 'server is up' );
+})
+
+//our first GET route
+app.get( '/devices', ( req, res )=>{
+   console.log( '/devises GET' );
+   res.send( devices ); 
 })
